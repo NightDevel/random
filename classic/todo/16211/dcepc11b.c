0bf29d93778450a402004e05a0c2043569c8d03f
madan <madan.pandiri@emerson.com>
2014-10-07 19:27:44 +0530

    added theory

--- classic/todo/16211/dcepc11b.c
+++ classic/todo/16211/dcepc11b.c
@@ -5,6 +5,7 @@ int factorial_mod(int N, int P)
 		return 0;
 	/* Wilsons theorem 
 	 * (P-1)! % P = -1 % P
+	 * when P is prime
 	 * ---------------------
 	 * (P+1)%P = 1 % P
 	 * (P+2)%p = 2 % P
@@ -15,9 +16,51 @@ int factorial_mod(int N, int P)
 	if (N == (P-1))
 		return P-1;
 
-	/* Remaining case N <= P-2 */
-	/* Lets use Wilson's theorem */
+	/* Remaining case N <= P-2 
+	   Lets use Wilson's theorem 
+	   1.2.3.4.....N % P
+	   1 - N = N terms
+	   
+	   if P is large, then 
+	   procedure 1: we have to check whether the straight approach
+	   ie. 1.2.3.4.5.6.7.8.9.... N (MOD P)
 
+	   or this
+	   procedure 2:
+	   let's assume 
+	   k(MOD P) = N!(mod P)
+	   k.(N+1)  . (N+2). (N+3) . (N+4)....(P-1)(MOD P) = N(N+1)(N+2)(N+3)...(P-1)(MOD P)
+										               = (P-1)!(MOD P)
+										               = -1(MOD P)
+	   k.(N+1-P) (N+2-P)                   (-2).(-1)   = -1(MOD P)
+	
+	   is better
+
+	   (P-(N+1)) < N => (P-1 < 2*N)
+	   
+	   If we choose procedure two then few more steps are required.
+	   Remeber we do not know what k is, which is what we have to find.
+
+
+	   Finding k:
+		fermat's theorem power(a, P-1) = 1(mod P), if a and P are relatively prime
+		let's assume that (N+1-P) (N+2-P) ...... -3 -2 -1 is a
+		then 
+		ka(MOD P)      = -1(MOD P)
+		-k.a(MOD P)    =  1(MOD P)
+		k.(-a)(MOD P)  =  1(MOD P)
+		k.(P-a)(MOD P) =  1(MOD P)   => (A)
+
+		set k = (P-a)^(P-2)
+		then (A) becomes (P-a)^(P-1)(MOD P) = 1(MOD P)
+						
+		due to fermats little theorem
+
+		finally find (P-a)^(P-2)(MOD P)
+
+		do repeated exponentiation and find the result.
+		*/
+		
 		long long mul = 1;
 		for (i = 1; (P+i) <= N; i++) {
 			mul *= mul * i;
