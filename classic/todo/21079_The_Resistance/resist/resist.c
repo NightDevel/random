0bc596408c0a9a3dc29ec94c14538912b24f8b5b
tin2012 <programmer.opensource@gmail.com>
2014-10-30 00:19:34 +0530

    need to finish

--- classic/todo/21079_The_Resistance/resist/resist.c
+++ classic/todo/21079_The_Resistance/resist/resist.c
@@ -1,5 +1,5 @@
-
 #include <stdio.h>
+#include <string.h>
 
 #ifdef DEBUG
 #define dprintf(fmt, ...) do { fprintf(stderr, fmt, __VA_ARGS__); } while (0)
@@ -7,21 +7,73 @@
 #define dprintf(fmt, ...) 
 #endif
 
-#define MAX_FIBS (2000000+10)
-int MAX_FIBS
-void calculate_fib()
+#define MAX_VAR 4
+
+typedef struct {
+	long long e[MAX_VAR][MAX_VAR];
+}matrix;
+
+matrix multiply(matrix P, int m, int n, matrix Q, int r, int s, int mod)
 {
-    F[0]
+	int i, j, l;
+	long long sum;
+	matrix D;
+	/* mxn * rxs = mxs */
+	for (i = 0; i < m; i++) {	  //final row value (m)
+		for (j = 0; j < s; j++) { //final column value (p)
+			D.e[i][j] = 0;
+			sum = 0;
+			for (l = 0; l < n; l++)  //n 
+				sum = (sum + P.e[i][l] * Q.e[l][j])%mod;
+			D.e[i][j] = sum;
+		}
+	}	
+	return D;
+}
+matrix power(matrix A, int k, int p, int mod)
+{
+	int i;
+	matrix B;
+	memset(&B, 0, sizeof(matrix));
+	for (i = 0; i < k; i++)
+		B.e[i][i] = 1;
+
+	while (p > 1) {
+		if (p % 2 == 0) {
+			A = multiply(A, k, k, A, k, k, mod);	 
+			p = p/2;
+		} else {
+			B = multiply(B, k, k, A, k, k, mod); 
+			p--;
+		}
+	}
+	B = multiply(B, k, k, A, k, k, mod); 
+	return B;
+}
+
+void calculate_fib(int n, int mod)
+{
+    matrix F, C;
+    F.e[0][0] = 1;
+    F.e[0][1] = 1;
+    F.e[1][0] = 1;
+    F.e[1][1] = 0;
+    C = power(F, 2, 2*n-1, mod) ;
+
+    printf("%lld/%lld\n", C.e[0][1], C.e[0][0]);
 
 }
+
 
 
 int main()
 {
     int t;
-    int N;
+    int N, M;
     scanf("%d", &t);
     while (t--){
+        scanf("%d %d", &N, &M);
+        calculate_fib(N, M);
     }
     return 0;
 }
